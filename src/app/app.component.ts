542c0c582d974e048ade6e592d199b87ffaa7c7f
Gautham G Ajith <gautham@innovatise.com>
2024-03-12 00:34:54 +0530

    Version 0.1 with basic function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pm';
+  password = ''
+
+  updatePassword(newPassword: string) {
+    this.password = newPassword;
+  }
 }
